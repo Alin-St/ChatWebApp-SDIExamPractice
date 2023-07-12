d2597d15b77c660b7e59942872139a98f8b5a48e
Alin Stanciu <alinstanciu2011@yahoo.com>
2023-07-12 20:01:20 +0300

    Solve 1 and 2

---
+++ Frontend-PlainJS/index.js
@@ -0,0 +1,14 @@
+function createChannel() {
+    document.getElementById("createChannelPopup").style.display = "block";
+    document.getElementById("createChannelOverlay").style.display = "block";
+}
+
+function createChannel_save() {
+    document.getElementById("createChannelPopup").style.display = "none";
+    document.getElementById("createChannelOverlay").style.display = "none";
+}
+
+function createChannel_cancel() {
+    document.getElementById("createChannelPopup").style.display = "none";
+    document.getElementById("createChannelOverlay").style.display = "none";
+}
